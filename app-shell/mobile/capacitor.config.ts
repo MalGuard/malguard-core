2b1278371f304266a91548ec2c655b3c7de075c9
Mohammad Reza Azarderakhsh <malguard.security@gmail.com>
2026-09-23 17:23:00 +0400

    Add app-shell/mobile/capacitor.config.ts

---
+++ app-shell/mobile/capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.github.malguard.app',
+  appName: 'MalGuard',
+  webDir: 'www',
+  server: {
+    url: 'https://malguard.github.io/',
+    cleartext: false,
+    allowNavigation: ['malguard.github.io']
+  }
+};
+
+export default config;
